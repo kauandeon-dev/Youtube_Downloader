d458c00591e81a22cc2fc0ba4abb343190734136
Kauan <kauandeon.dev@gmail.com>
2026-03-25 00:02:03 -0300

    Adicionado validação na URL e tratamento de erro

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-url = input("Digite a URL do vídeo do Youtube: ")
+print("Digite a URL do vídeo do Youtube")
+url = input("URL: ").strip()
 
+if "youtube.com" not in url and "youtu.be" not in url:
+    print("Link inválido")
+    exit()
+
+try:
+    yt = YouTube(url)
+except Exception:
+    print("Vídeo inválido ou indisponível")
+    exit()
+    
 yt = YouTube(url, on_progress_callback=on_progress)
 ys = yt.streams.get_highest_resolution()
 print(yt.title)
